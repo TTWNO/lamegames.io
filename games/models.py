994f6132c7a486a366deb5d59db353d2979b4579
Tait Hoyem <tait@tait.tech>
2020-08-22 16:17:23 +0000

    Add some basic models for games.

--- games/models.py
+++ games/models.py
@@ -1,3 +1,14 @@
+import json
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    game = models.CharField(max_length=32)
+
+    def toJson(self):
+        return JSON.dumps({'game': self.game})
+
+class ActiveUser(models.Model):
+    username = models.CharField(max_length=32)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
