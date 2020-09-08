42481a42387c1efcd6a8d90eacea7cab0caba651
Tait Hoyem <code@tait.tech>
2020-09-08 14:27:41 +0000

    simplify code; remove check for already connected user

--- games/views.py
+++ games/views.py
@@ -1,32 +1,25 @@
 from django.shortcuts import (
     render, HttpResponse, redirect
 )
-from .models import *
+from django.urls import reverse
+from .models import Room, ActiveUser
 
 # Create your views here.
 def rps(request):
-    return render(request, 'games/rps_choose.html', {})
+    return render(request, 'games/rps/rps_choose.html', {})
 
-def rps_join(request):
-    if request.method == 'POST':
-        return render(request, 'games/rps.html', {
-            'ROOM_ID': request.POST['rid']
-        })
-    else:
-        return render(request, 'games/rps_join.html', {})
+def rps_select(request):
+    return render(request, 'games/rps/rps_join.html', {'all_rooms': Room.objects.all()})
+
+def rps_join(request, room_id):
+    return render(request, 'games/rps/rps.html', {'ROOM_ID': room_id})
 
 def rps_create(request):
     username = request.user.username
-    # if the current user is in another room. Remove them.
-    for x in Room.objects.all():
-        print("Checking room...")
-        if len(ActiveUser.objects.filter(room=x, username=username)) > 0:
-            print("User in another room. Deleting")
-            x.delete()
     
     room = Room(game="rps", id=username)
     active_user = ActiveUser(username=username, room=room)
     room.save()
     active_user.save()
 
-    return redirect('rps/join')
+    return redirect(reverse('rps/join', args=[username]))
