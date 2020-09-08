3bd1c4a4d0b13d0a7b0374473451ff897e4ef659
Tait Hoyem <code@tait.tech>
2020-09-08 04:16:07 +0000

    Add rps/ routs

--- games/urls.py
+++ games/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rps/', views.rps, name="rps")
+    path('rps/', views.rps, name="rps"),
+    path('rps/join', views.rps_join, name="rps/join"),
+    path('rps/create', views.rps_create, name="rps/create")
 ]
