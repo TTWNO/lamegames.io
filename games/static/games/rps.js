5ddc1bd9b0bfbd60b06a53e4671c499abe56ba2e
Tait Hoyem <code@tait.tech>
2020-09-08 04:16:34 +0000

    Implement notifications for users via alert()

--- games/static/games/rps.js
+++ games/static/games/rps.js
@@ -4,9 +4,37 @@ const RPSSocket = new WebSocket(
     + '://'
     + window.location.host
     + '/rps/'
+    + ROOM_ID
 );
 
 RPSSocket.onmessage = function(e) {
     console.log("Receiving...");
+    console.log(e.data);
     console.log(JSON.parse(e.data));
+
+    data = JSON.parse(e.data);
+    message = data.message;
+    if (data.event === 'warning')
+    {
+        alert(message);
+    } else if (data.event == 'game_over') {
+        if (message === 'tie')
+        {
+            alert("Tie!");
+        } else {
+            alert("The winner is: " + message.winner);
+        }
+    }
+}
+
+for (btn of document.querySelectorAll(".rpsbtn"))
+{
+    btn.addEventListener('click', e => {
+        alert("You pressed " + e.target.id);
+        RPSSocket.send(
+            JSON.stringify({
+                'choice': e.target.id
+            })
+        );
+    })
 }
