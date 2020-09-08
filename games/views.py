dc309be2305307d87204a2fad49642d67b417c23
Tait Hoyem <code@tait.tech>
2020-09-08 04:15:42 +0000

    Add RPS setup functionality

--- games/views.py
+++ games/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import (
-    render, HttpResponse
+    render, HttpResponse, redirect
 )
+from .models import *
 
 # Create your views here.
 def rps(request):
-    return render(request, 'games/rps.html', {})
+    return render(request, 'games/rps_choose.html', {})
+
+def rps_join(request):
+    if request.method == 'POST':
+        return render(request, 'games/rps.html', {
+            'ROOM_ID': request.POST['rid']
+        })
+    else:
+        return render(request, 'games/rps_join.html', {})
+
+def rps_create(request):
+    username = request.user.username
+    # if the current user is in another room. Remove them.
+    for x in Room.objects.all():
+        print("Checking room...")
+        if len(ActiveUser.objects.filter(room=x, username=username)) > 0:
+            print("User in another room. Deleting")
+            x.delete()
+    
+    room = Room(game="rps", id=username)
+    active_user = ActiveUser(username=username, room=room)
+    room.save()
+    active_user.save()
+
+    return redirect('rps/join')
